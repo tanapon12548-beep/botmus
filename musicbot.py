0359debb4d82cfc8a0b5dec210d1669f311e7fff
thanapon <tanapon12548@gamil.com>
2026-08-10 01:31:27 +0700

    fix: switch from PCM to Opus audio stream to fix premature cutoff on low CPU instances

--- musicbot.py
+++ musicbot.py
@@ -167,12 +167,22 @@ def get_queue(guild_id):
         song_queues[guild_id] = deque()
     return song_queues[guild_id]
 
-class YTDLSource(discord.PCMVolumeTransformer):
-    def __init__(self, source, *, data, volume=0.5):
-        super().__init__(source, volume)
+class YTDLSource(discord.AudioSource):
+    def __init__(self, source, *, data):
+        self.source = source
         self.data = data
         self.title = data.get('title')
         self.url = data.get('url')
+        self.filename = None
+
+    def read(self):
+        return self.source.read()
+
+    def is_opus(self):
+        return self.source.is_opus()
+
+    def cleanup(self):
+        self.source.cleanup()
 
     @classmethod
     async def from_url(cls, url, *, loop=None):
@@ -191,7 +201,9 @@ class YTDLSource(discord.PCMVolumeTransformer):
             # กรณีหา filepath ไม่เจอ ให้ลองใช้ค่า ext ประกอบชื่อ
             filename = ytdl.prepare_filename(data)
             
-        player = cls(discord.FFmpegPCMAudio(filename, executable=FFMPEG_PATH, **ffmpeg_options), data=data)
+        # ใช้ FFmpegOpusAudio เพื่อลดการใช้ CPU และแก้ปัญหาเพลงตัดจบ
+        audio_source = await discord.FFmpegOpusAudio.from_probe(filename, executable=FFMPEG_PATH, **ffmpeg_options)
+        player = cls(audio_source, data=data)
         player.filename = filename
         return player
 
